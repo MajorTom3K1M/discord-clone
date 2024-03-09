272337250b650fd0c32c598fe06cfd64a7d742e9
MajorTom <invaderzgame@gmail.com>
2024-03-09 17:16:32 +0700

    [FIX] Change Route Name to Follow API Naming Convention

--- backend/internal/routes/server_routes.go
+++ backend/internal/routes/server_routes.go
@@ -7,16 +7,16 @@ import (
 )
 
 func ServerRoutes(protected *gin.RouterGroup, serverHandler *handlers.ServerHandler) {
-	serverGroup := protected.Group("/server")
-	{
-		serverGroup.POST("", serverHandler.CreateServer)
-		serverGroup.GET("/:serverId", serverHandler.GetServer)
-		serverGroup.GET("/:serverId/details", serverHandler.GetServerDetails)
-		serverGroup.PATCH("/:serverId/invite-code", serverHandler.UpdateServerInviteCode)
-	}
-
 	serversGroup := protected.Group("/servers")
 	{
 		serversGroup.GET("", serverHandler.GetServers)
+		serversGroup.GET("/invite-code/:inviteCode", serverHandler.GetServerByInviteCode)
+		serversGroup.GET("/:serverId", serverHandler.GetServer)
+		serversGroup.GET("/:serverId/details", serverHandler.GetServerDetails)
+
+		serversGroup.POST("", serverHandler.CreateServer)
+		serversGroup.POST("/invite-code/:inviteCode/members", serverHandler.UpdateServerMember)
+
+		serversGroup.PATCH("/:serverId/invite-code", serverHandler.UpdateServerInviteCode)
 	}
 }
