b2552f738e0423e0b6643fd583563ccfacfd88d6
MajorTom <invaderzgame@gmail.com>
2024-03-17 01:52:06 +0700

    [ADD] Leave Server Feature

--- frontend/hooks/useModalStore.ts
+++ frontend/hooks/useModalStore.ts
@@ -1,7 +1,7 @@
 import { Server } from '@/types/models';
 import { create } from 'zustand';
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer";
 
 interface ModalData {
     server?: Server
