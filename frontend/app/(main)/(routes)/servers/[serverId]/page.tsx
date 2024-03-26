c685ca8920186a352ef215245c7379a25f3b83e2
MajorTom <invaderzgame@gmail.com>
2024-03-26 19:25:08 +0700

    [ADD] Member & Converstaion Initial Page

--- frontend/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ frontend/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,50 @@
-const ServerIdPage = () => {
-    return (  
-        <div>Server ID Page</div>
-    );
+import axios from "@/utils/axios";
+
+import { currentProfile } from "@/lib/currentProfile";
+import { Server } from "@/types/models";
+import { cookies } from "next/headers";
+import { redirect, useSearchParams } from 'next/navigation';
+
+interface ServerIdPageProps {
+    params: {
+        serverId: string;
+    }
 }
- 
+
+const getServerGeneralChannel = async (serverId: string) => {
+    try {
+        const server = await axios.get<{
+            message: string,
+            server: Server
+        }>(`/servers/${serverId}/channels/default`, {
+            headers: {
+                Cookie: cookies().toString()
+            }
+        });
+        return server.data.server
+    } catch (err) {
+        return null
+    }
+};
+
+const ServerIdPage = async ({
+    params
+}: ServerIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirect("/sign-in");
+    }
+
+    const server = await getServerGeneralChannel(params.serverId);
+
+    const initialChannel = server?.channels?.[0];
+
+    if (!initialChannel || initialChannel?.name !== "general") {
+        return null;
+    }
+
+    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
+}
+
 export default ServerIdPage;
