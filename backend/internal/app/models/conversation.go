962d372f37f9a5ab63f10f4f62e07a952518cd19
MajorTom <invaderzgame@gmail.com>
2024-03-28 20:52:31 +0700

    [ADD] Index In Conversation Model

--- backend/internal/app/models/conversation.go
+++ backend/internal/app/models/conversation.go
@@ -9,10 +9,10 @@ import (
 
 type Conversation struct {
 	ID             uuid.UUID `gorm:"type:uuid;primary_key;"`
-	MemberOneID    uuid.UUID
-	MemberOne      Member `gorm:"foreignKey:MemberOneID;references:ID;onDelete:CASCADE"`
-	MemberTwoID    uuid.UUID
-	MemberTwo      Member `gorm:"foreignKey:MemberTwoID;references:ID;onDelete:CASCADE"`
+	MemberOneID    uuid.UUID `gorm:"index:,unique"`
+	MemberOne      Member    `gorm:"foreignKey:MemberOneID;references:ID;onDelete:CASCADE"`
+	MemberTwoID    uuid.UUID `gorm:"index:,unique;index"`
+	MemberTwo      Member    `gorm:"foreignKey:MemberTwoID;references:ID;onDelete:CASCADE"`
 	DirectMessages []DirectMessage
 	CreatedAt      time.Time `gorm:"default:CURRENT_TIMESTAMP"`
 	UpdatedAt      time.Time
