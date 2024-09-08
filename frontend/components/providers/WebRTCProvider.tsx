31dce27b3dd1651d79015e703af19a5d49af6ccf
MajorTom <invaderzgame@gmail.com>
2024-09-08 19:50:29 +0700

    [EDIT] Remove IceServer from Client and Test Using Only One IceServer on Server

--- frontend/components/providers/WebRTCProvider.tsx
+++ frontend/components/providers/WebRTCProvider.tsx
@@ -59,36 +59,7 @@ export const WebRTCProvider = ({
 
     const pcRef = useRef<RTCPeerConnection | null>(null);
 
-    const configuration: RTCConfiguration = {
-        iceServers: [
-            {
-                urls: [
-                    "stun:stun.l.google.com:19302",
-                    "stun:stun.because-why-not.com:443",
-                    "stun:stun01.sipphone.com",
-                    "stun:stun.ekiga.net",
-                    "stun:stun.fwdnet.net",
-                    "stun:stun.ideasip.com",
-                    "stun:stun.iptel.org",
-                    "stun:stun.rixtelecom.se",
-                    "stun:stun.schlund.de",
-                    "stun:stun.l.google.com:19302",
-                    "stun:stun1.l.google.com:19302",
-                    "stun:stun2.l.google.com:19302",
-                    "stun:stun3.l.google.com:19302",
-                    "stun:stun4.l.google.com:19302",
-                    "stun:stunserver.org",
-                    "stun:stunserver.org:3478",
-                    "stun:stun.softjoys.com",
-                    "stun:stun.voiparound.com",
-                    "stun:stun.voipbuster.com",
-                    "stun:stun.voipstunt.com",
-                    "stun:stun.voxgratia.org",
-                    "stun:stun.xten.com",
-                ]
-            }
-        ]
-    };
+    const configuration: RTCConfiguration = {};
 
     useEffect(() => {
         const handleMessage = (event: MessageEvent) => {
